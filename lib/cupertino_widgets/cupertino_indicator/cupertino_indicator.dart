26ebe8b2eefe9f9c2f60b412a4ef224969e7b6b4
sinanhaci <77547319+sinanhaci@users.noreply.github.com>
2022-10-30 21:08:44 +0300

    Update cupertino_indicator.dart

--- lib/cupertino_widgets/cupertino_indicator/cupertino_indicator.dart
+++ lib/cupertino_widgets/cupertino_indicator/cupertino_indicator.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/cupertino.dart';
 
-
 import '../../enums/indicator_type.dart';
 import '../../models/indicator_model.dart';
 
 class CupertinoIndicatorWidget extends StatelessWidget {
   final IndicatorModel constructors;
-  const CupertinoIndicatorWidget({super.key,required this.constructors});
+  const CupertinoIndicatorWidget({super.key, required this.constructors});
 
   @override
   Widget build(BuildContext context) {
@@ -16,9 +15,17 @@ class CupertinoIndicatorWidget extends StatelessWidget {
   Widget _getWidgetByIndicatorType() {
     switch (constructors.indicatorType) {
       case IndicatorType.center:
-        return Center(child: CupertinoActivityIndicator(color: constructors.color));
+        return Center(
+            child: CupertinoActivityIndicator(color: constructors.color));
       case IndicatorType.inStack:
-        return Positioned.fill(child: Center(child: CupertinoActivityIndicator(color: constructors.color)));
+        return Positioned.fill(
+          child: Container(
+            color: Theme.of(context).scaffoldBackgroundColor.withOpacity(0.25),
+            child: Center(
+              child: CupertinoActivityIndicator(color: constructors.color),
+            ),
+          ),
+        );
     }
   }
 }
