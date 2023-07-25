2fcb6374a14774a243ba5ec9a89a1a4c706b5875
sinanhaci <77547319+sinanhaci@users.noreply.github.com>
2023-07-25 14:36:58 +0300

    Material action sheet scroll issues fixed.

--- lib/material_widgets/material_action_sheet/material_action_sheet.dart
+++ lib/material_widgets/material_action_sheet/material_action_sheet.dart
@@ -9,37 +9,53 @@ class MaterialActionSheet extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var content = [
-      if(properties.title != null || properties.content != null)
-      ...[ListTile(
-          tileColor: properties.androidInfoTileColor,
-          title: _getTextWidgetByConstructors(properties.title,style?.titleStyle),
-          subtitle: _getTextWidgetByConstructors(properties.content,style?.messageStyle),
-          trailing: properties.androidShowCloseButton ? 
-            IconButton(
-              visualDensity: VisualDensity.compact,
-              icon: const Icon(Icons.close,size: 30),
-              onPressed: () => Navigator.pop(context),
-            ) : null,
-        ),
-      const Divider(height: 1,)],
-      Expanded(
-        child: ListView(
-          children: properties.actions
-          .map((e) => ListTile(
-                visualDensity: style?.androidVisualDensity,
-                minLeadingWidth: 10,
-                onTap: e.onPress,
-                title: Text(e.text, style: e.isDefaultAction ? style?.isDefaultActionStyle : e.isDestructiveAction ? (style?.isDestructiveActionStyle ?? const TextStyle(color: Colors.red)) : style?.actionButtonStyle),
-                leading: e.icon == null ? null : Icon(e.icon,color: style?.iconColor ?? (e.isDestructiveAction ? Colors.red : Colors.blue),size: style?.iconSize),
-              ))
-          .toList()
-        ),
+
+      Column(
+        mainAxisSize: MainAxisSize.min,
+        children: properties.actions
+        .map((e) => ListTile(
+              visualDensity: style?.androidVisualDensity,
+              minLeadingWidth: 10,
+              onTap: e.onPress,
+              title: Text(e.text, style: e.isDefaultAction ? style?.isDefaultActionStyle : e.isDestructiveAction ? (style?.isDestructiveActionStyle ?? const TextStyle(color: Colors.red)) : style?.actionButtonStyle),
+              leading: e.icon == null ? null : Icon(e.icon,color: style?.iconColor ?? (e.isDestructiveAction ? Colors.red : Colors.blue),size: style?.iconSize),
+            ))
+        .toList()
       )
     ];
     return SafeArea(
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: content,
+      child: Stack(
+        children: [
+          Padding(
+            padding: properties.title != null || properties.content != null ? const EdgeInsets.only(top: 70) : EdgeInsets.zero,
+            child: ListView(
+              shrinkWrap: true,
+              children: content,
+            ),
+          ),
+          Positioned(
+            top: 0,
+            right: 0,
+            left: 0,
+            child: Column(
+              children: [
+                if(properties.title != null || properties.content != null)
+                ...[ListTile(
+                    tileColor: properties.androidInfoTileColor,
+                    title: _getTextWidgetByConstructors(properties.title,style?.titleStyle),
+                    subtitle: _getTextWidgetByConstructors(properties.content,style?.messageStyle),
+                    trailing: properties.androidShowCloseButton ? 
+                      IconButton(
+                        visualDensity: VisualDensity.compact,
+                        icon: const Icon(Icons.close,size: 30),
+                        onPressed: () => Navigator.pop(context),
+                      ) : null,
+                  ),
+                const Divider(height: 1,)],
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
